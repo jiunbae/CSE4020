60bc26f06cade4d55a29f31a68b57ca4a6a1064e
bae jiun, maybe <alice.maydev@gmail.com>
2018-06-02 22:08:27 +0900

    Update tests.obj for load object

---
+++ assignment3/tests/test_obj.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+""" tests.obj
+    ---------------
+    Test of OBJ
+    :author: MaybeS(maytryark@gmail.com)
+"""
+
+import pytest
+from lib.obj import OBJ
+
+def test_objload() :
+    """ Testing for load obj file
+    """
+
+    obj = OBJ.read_obj('./obj/cube-tri.obj')
+
+    assert all(isinstance(it, tuple) for it in obj._vertices)
+    assert all(isinstance(it, tuple) for it in obj._normals)
+    assert all(isinstance(it, tuple) for it in obj._texcoords)
+    assert all(isinstance(it, tuple) for it in obj._faces)
