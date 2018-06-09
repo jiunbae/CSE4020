b6535ed98110928b0783bba64d5720e90023f432
bae jiun, maybe <alice.maydev@gmail.com>
2018-06-10 03:57:30 +0900

    Update lib.lights and lib.obj
    Fix multiple lights enable
    Fix object loader

--- assignment3/lib/light.py
+++ assignment3/lib/light.py
@@ -4,6 +4,14 @@ from OpenGL.GLU import *
 from .mode import Mode
 
 class Light:
+    class COLOR:
+        def __init__(self,
+                    ambient = (0,0,0,0),
+                    diffuse = (0,0,0,0),
+                    specular= (0,0,0,0)):
+            self.ambient = ambient
+            self.diffuse = diffuse
+            self.specular = specular
     index = Mode([GL_LIGHT0, 
                   GL_LIGHT1,
                   GL_LIGHT2,
@@ -12,26 +20,38 @@ class Light:
                   GL_LIGHT5,
                   GL_LIGHT6,
                   GL_LIGHT7])
-
-    class COLOR:
-        def __init__(self, ambient=(0,0,0,0), diffuse=(0,0,0,0), specular=(0,0,0,0)):
-            self.ambient = ambient
-            self.diffuse = diffuse
-            self.specular = specular
+    lights = list()
+    obj = COLOR((1., 1., 1., 1.),
+                (1., 1., 1., 1.),
+                (1., 1., 1., 1.))
 
     def __init__(self, args):
         self.index = Light.index.get(next=True)
-        print (self.index)
         self.pos = args['pos']
-        self.light = Light.COLOR(args['ambient'],args['diffuse'],args['specular'])
+        self.light = Light.COLOR(args['ambient'],
+                                 args['diffuse'],
+                                 args['specular'])
+        Light.lights.append(self)
 
-    def render(self):
-        glPushMatrix()
-
+    def _render(self):
+        glEnable(self.index)
         glLightfv(self.index, GL_POSITION, self.pos)
-
-        glPopMatrix()
-
         glLightfv(self.index, GL_AMBIENT, self.light.ambient)
         glLightfv(self.index, GL_DIFFUSE, self.light.diffuse)
         glLightfv(self.index, GL_SPECULAR, self.light.specular)
+
+    @staticmethod
+    def objectColor(ambient =(1., 1., 1., 1.),
+                    diffuse =(1., 1., 1., 1.),
+                    specular=(1., 1., 1., 1.)):
+        Light.obj = Light.COLOR(ambient, diffuse, specular)
+
+    @staticmethod
+    def render():
+        glEnable(GL_LIGHTING)
+
+        for light in Light.lights:
+            light._render()
+
+        glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, Light.obj.diffuse)
+
--- assignment3/lib/obj.py
+++ assignment3/lib/obj.py
@@ -1,54 +1,82 @@
+from os import path
 from collections import OrderedDict
 from itertools import chain
 
 import numpy as np
+from OpenGL.GL import *
+from OpenGL.GLU import *
 
 class OBJ:
     _buffer = type('buffer', (), {
-        'vertex': np.zeros((0, 3)),
-        'normal': np.zeros((0, 3)),
-        'texcoord': np.zeros((0, 3))
+        'vertex'    : np.empty((0, 3)),
+        'normal'    : np.empty((0, 3)),
+        'texcoord'  : np.empty((0, 3)),
     })
 
-    def __init__(self):
-        self._faces = list()
-        self.values = OBJ._buffer()
-        self.buffer = OBJ._buffer()
+    def __init__(self, filename=''):
+        self.filename   = filename
+        self.index      = None
+        self.indexes    = dict()
+        self.faces      = list()
+        self.values     = OBJ._buffer()
+        self.buffer     = OBJ._buffer()
+
+    def __str__(self):
+        return 'Object Information\n' +\
+               ('from file: {}\n'.format(self.filename) if self.filename else '') +\
+               '\tTotal number of faces: {}\n'.format(len(self.faces)) +\
+               '\tNumber of faces with 3 vertices: {}\n'.format(0) +\
+               '\tNumber of faces with 4 vertices: {}\n'.format(0) +\
+               '\tNumber of faces with more than 4 vertices: {}\n'.format(0)
+
+    def render(self):
+        glBegin(GL_POLYGON)
+        for v, n, t in zip(self.buffer.vertex, self.buffer.normal, self.buffer.texcoord):
+            if self.values.normal.size:
+                glNormal3fv(n)
+            if self.values.texcoord.size :
+                glTexCoord2fv(t)
+            glVertex3fv(v)
+        glEnd()
 
     @staticmethod
     def read_obj(filename):
-        obj = OBJ()
+        obj = OBJ(path.basename(filename))
 
         def _type(v):
-            try:
-                return tuple(map(int, v))
+            try: return int(v or 0)
             except ValueError:
-                return tuple(map(float, v))
+                return float(v)
+
         def _update(tar, val):
             setattr(obj.values, tar, np.append(getattr(obj.values, tar), [val], axis=0))
 
         parse = lambda t, *v: {
-            'v' : lambda v: _update('vertex', _type(v)),
-            'vn': lambda v: _update('normal', _type(v)),
-            'vt': lambda v: _update('texcoord', _type(v)),
-            'f' : lambda v: obj._faces.append(tuple(_type(z.split('//')) for z in v)),
-        }.get(t, lambda v: None)(v)
+            'v' : lambda v: _update('vertex', tuple(map(_type, v))),
+            'vn': lambda v: _update('normal', tuple(map(_type, v))),
+            'vt': lambda v: _update('texcoord', tuple(map(_type, v))),
+            'f' : lambda v: obj.faces.append([tuple(map(_type, z.split('/'))) for z in v]),
+        }.get(t,  lambda v: None)(v)
 
         with open(filename) as file:
             any(parse(*line) for line in map(str.split, file))
 
-        indexes = OrderedDict.fromkeys(chain(*obj._faces))
+        # obj.indexes = {f: i for i, f in enumerate(set(chain(*obj.faces)))}
+        count = 0
+        for index in chain(*obj.faces):
+            if index not in obj.indexes:
+                obj.indexes[index] = count
+                count +=1 
 
-        obj.buffer.vertex = np.ndarray((len(indexes), 3), np.float32)
-        obj.buffer.normal = np.ndarray((len(indexes), 3), np.float32)        
-        obj.buffer.texcoord = np.ndarray((len(indexes), 3), np.float32)
-        for index, value in enumerate(indexes):
-            obj.buffer.vertex[index] = obj.values.vertex[value[0]-1]
-            obj.buffer.normal[index] = obj.values.normal[value[0]-1]
-            obj.buffer.texcoord[index] = obj.values.texcoord[value[0]-1]
+        obj.faces = np.asarray(obj.faces)
+        obj.buffer.vertex   = np.empty((len(obj.indexes), 3), np.float32)
+        obj.buffer.normal   = np.empty((len(obj.indexes), 3), np.float32)
+        obj.buffer.texcoord = np.empty((len(obj.indexes), 2), np.float32)
 
-        print (obj.buffer.vertex)
-        print (obj.buffer.normal)
-        print (obj.buffer.texcoord)
+        for index, value in obj.indexes.items():
+            obj.buffer.vertex[value]   = obj.values.vertex[index[0]-1]   if obj.values.vertex.size else 0
+            obj.buffer.normal[value]   = obj.values.normal[index[2]-1]   if obj.values.normal.size else 0
+            obj.buffer.texcoord[value] = obj.values.texcoord[index[1]-1] if obj.values.texcoord.size else 0
+        obj.index = np.apply_along_axis(lambda x: obj.indexes[tuple(x)], 2, obj.faces)
 
         return obj
