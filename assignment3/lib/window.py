5f7b5165c33589595f12b463c89c570a2f148e92
bae jiun, maybe <alice.maydev@gmail.com>
2018-06-11 02:11:10 +0900

    Update windows.frame

--- assignment3/lib/window.py
+++ assignment3/lib/window.py
@@ -1,4 +1,6 @@
 import glfw
+from OpenGL.GL import *
+from OpenGL.GLU import *
 
 from .obj import OBJ
 
@@ -8,6 +10,19 @@ class Window:
         self.height = height
         self.name = name
         self.context = glfw.create_window(width, height, name, None, None)
+
+    def frame(self):
+        glBegin(GL_LINES)
+        glColor3ub(255,0,0)
+        glVertex3fv([0.,0.,0.])
+        glVertex3fv([1.,0.,0.])
+        glColor3ub(0,255,0)
+        glVertex3fv([0.,0.,0.])
+        glVertex3fv([0.,1.,0.])
+        glColor3ub(0,0,255)
+        glVertex3fv([0.,0.,0])
+        glVertex3fv([0.,0.,1.])
+        glEnd()
 
     def _callback_key(self, window, key, scancode, action, mods):
         pass
